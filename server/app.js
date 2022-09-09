8e31d919ca7bd58a09857b974f590a45a3c0df36
johnchri <johnchri@student.chalmers.se>
2022-09-09 15:26:24 +0200

    Add preliminary test user schema. Add models and controllers folders.

--- server/app.js
+++ server/app.js
@@ -4,9 +4,15 @@ var morgan = require('morgan');
 var path = require('path');
 var cors = require('cors');
 var history = require('connect-history-api-fallback');
+const bodyParser = require('body-parser');
+
+var User = require('./models/user')
+
+// Create Express app
+var app = express();
 
 // Variables
-var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/animalDevelopmentDB';
+var mongoURI = process.env.MONGODB_URI || 'mongodb+srv://webdev32:webdev32pass@cluster0.ay1qyti.mongodb.net/?retryWrites=true&w=majority';
 var port = process.env.PORT || 3000;
 
 // Connect to MongoDB
@@ -19,8 +25,6 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, 
     console.log(`Connected to MongoDB with URI: ${mongoURI}`);
 });
 
-// Create Express app
-var app = express();
 // Parse requests of content-type 'application/json'
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -38,6 +42,56 @@ app.get('/api', function(req, res) {
 // Catch all non-error handler for api (i.e., 404 Not Found)
 app.use('/api/*', function (req, res) {
     res.status(404).json({ 'message': 'Not Found' });
+});
+
+//test routes
+app.post('/add-user', (req, res) => {
+var user = new User({
+    name : "jeet",
+    phoneNumber : 882844498,
+    PNR: 9219869902,
+    location: "Addresssssss",
+    email: "email@gmail.com"
+});
+    user.save()
+    .then((result) => {
+        res.send(result)
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+})
+
+var users = []; // Users storage array
+app.post('/create-user', function(req, res) {
+    var new_user = {
+        _id : users.length,
+        name : req.body.name,
+        phoneNumber : req.body.phoneNumber,
+        PNR: req.body.PNR,
+        location: req.body.location,
+        email: req.body.email
+    }
+    users.push(new_user);
+    res.json(new_user);
+});
+
+app.get('/users', function(req, res, next) {
+    User.find(function(err, users) {
+        if (err) { return next(err); }
+        res.json({"users": users});
+    });
+});
+
+app.get('/users/_id', function(req, res, next) {
+    var id = req.params.id;
+    User.findById(req.params.id, function(err, user) {
+        if (err) { return next(err); }
+        if (user == null) {
+            return res.status(404).json({"message": "User not found"});
+        }
+        res.json(user);
+    });
 });
 
 // Configuration for serving frontend in production mode
---
+++ server/models/user.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+/*
+var userSchema = new Schema({
+    fullName : [{firstName: String, middleName: String, lastName: String}],
+    phoneNumber : {type: Number, unique: true},
+    PNR: {type : Number, unique: true},
+    location: [{city : String, postNR: Number, address: String}],
+    email: {type : String, required: true, unique: true}
+});
+*/
+
+var userSchema = new Schema({
+    name : {type:String},
+    phoneNumber : {type: Number},
+    location: {type: String},
+    email: {type : String}
+});
+
+
+var User = mongoose.model('users', userSchema);
+module.exports = User;
