91bbaf5e4ffcb8da0bd53dbf951e6a43f43bceda
Aleksey Zorin <aleksey@student.chalmers.se>
2022-10-11 15:20:56 +0200

    Item review user input stuff

--- client/src/views/Item.vue
+++ client/src/views/Item.vue
@@ -22,6 +22,29 @@
         </body>
       </b-col>
     </b-row>
+
+    <!-- User input box for item review. -->
+    <span>Write a review for this item:</span>
+<p style="white-space: pre-line;">{{ message }}</p>
+<textarea v-model="message" placeholder="This product..."></textarea>
+
+<!-- User input selection of item grade. -->
+<div>Grade this item: {{ selected }}</div>
+
+<select v-model="selected">
+  <option disabled value="">Please select one</option>
+  <option>5</option>
+  <option>4</option>
+  <option>3</option>
+  <option>2</option>
+  <option>1</option>
+</select>
+
+<!-- "Submit Review" button. -->
+<div id="app">
+  <p>{{ message }}</p>
+  <button v-on:click="submitReview">Submit Review</button>
+</div>
     <b-row>
       <b-col id="reviews">
         <h5>Reviews for {{ item.itemName }}:</h5>
@@ -95,6 +118,7 @@ export default {
   },
   data() {
     return {
+      message: '', // needs to be hidden from user?
       user: {
         fullName: '',
         userName: '',
