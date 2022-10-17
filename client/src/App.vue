3e8cf9b644a49e6c1a37a68298361e5a1ef0d7a9
Carl D <carlthur@student.chalmers.se>
2022-10-17 11:58:36 +0200

    footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div
-      class="navbar"
-      v-if="!['login', 'register'].includes(this.$route.name)"
-    >
+    <div class="navbar" v-if="!['login', 'register'].includes(this.$route.name)">
       <div class="navbar-left">
         <b-button id="button" to="/home">Home</b-button>
         <b-button id="button" to="/listings">Listings</b-button>
@@ -17,23 +14,48 @@
         <b-button id="button" v-if="loggedIn" to="/my-account">My Account</b-button>
       </div>
     </div>
-    <!-- Render the content of the current page view -->
     <router-view />
-    <footer class="mt-auto">
-    <div>
-      <b-card
-        overlay
-        img-src="https://www.rseconf.org/wp-content/uploads/2019/02/footer-bg.png"
-        img-alt="Card Image"
-        img-width="100%"
-        max-height="40rem">
-        <b-card-text>
-          <a href="http://localhost:8080/admin-login">Admin Login</a>
-        </b-card-text>
-      </b-card>
-    </div>
-  </footer>
+
+    <footer class="text-center text-white" style="background-color: #3f51b5">
+      <div class="container">
+        <section class="mt-5">
+          <div class="row text-center d-flex justify-content-center pt-5">
+            <div class="col-md-2">
+              <h6 class="text-uppercase font-weight-bold">
+                <a href="https://en.wikipedia.org/wiki/The_Three_Stooges" class="text-white">About us</a>
+              </h6>
+            </div>
+
+            <div class="col-md-2">
+              <h6 class="text-uppercase font-weight-bold">
+                <a href="http://localhost:8080/listings" class="text-white">Listings</a>
+              </h6>
+            </div>
+
+            <div class="col-md-2">
+              <h6 class="text-uppercase font-weight-bold">
+                <a href="https://www.amazon.se/Coding-Development-All-One-Dummies/dp/1119473926/ref=asc_df_1119473926/?tag=shpngadsglede-21&linkCode=df0&hvadid=476505640779&hvpos=&hvnetw=g&hvrand=13015083246108035693&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1012511&hvtargid=pla-489073989775&psc=1"
+                class="text-white">Help</a>
+              </h6>
+            </div>
+
+            <div class="col-md-2">
+              <h6 class="text-uppercase font-weight-bold">
+                <a href="#!" class="text-white">Contact</a>
+              </h6>
+            </div>
+            <div class="col-md-2">
+              <h6 class="text-uppercase font-weight-bold">
+                <a href="http://localhost:8080/admin-login" class="text-white">Administrator Login</a>
+              </h6>
+            </div>
+          </div>
+        </section>
+        <hr class="my-5" />
+      </div>
+    </footer>
   </div>
+
 </template>
 
 <script>
@@ -53,7 +75,6 @@ export default {
 </script>
 
 <style>
-
 * {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
 }
@@ -85,12 +106,14 @@ export default {
 #button:active {
   border: none;
 }
+
 .navbar-left {
   display: flex;
   align-items: center;
   flex-wrap: wrap;
   flex-direction: wrap;
 }
+
 .navbar-center {
   display: flex;
   align-items: center;
